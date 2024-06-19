476e4e83d1fb52ab803b05f9f38e09d6f7e54a38
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-19 13:00:11 -0300

    Styled Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import "./Header.scss";
+
 import {
   SignedIn,
   SignedOut,
@@ -9,25 +11,25 @@ export const Header = () => {
   return (
     <header className="header">
       <h1 className="header__title">
-        <img src="/svg/logo.svg" alt="Write Logo" width="30" height="30" />
+        <img src="/svg/logo.svg" alt="Write Logo" width="50" height="50" />
       </h1>
 
       <nav className="header__nav">
         <ul className="header__lists">
           <li className="header__list">
-            <button className="header__write">
-                <img src="/svg/write.svg" alt="Write Logo" width="30" height="30" />
+            <button className="header__write button">
+                <img src="/svg/write.svg" alt="Write Logo" width="24" height="24" className="header__write--logo"/>
                 Write
             </button>
           </li>
-          <li>
-          <button>
-                <img src="/svg/bell.svg" alt="Bell Logo" width="30" height="30" />
+          <li className="header__list">
+            <button className="header__notification button">
+                  <img src="/svg/bell.svg" alt="Bell Logo" width="24" height="24" />
             </button>
           </li>
-          <li>
+          <li className="header__list">
             <SignedOut>
-              <SignInButton />
+              <SignInButton className="button"/>
             </SignedOut>
             <SignedIn>
               <UserButton />

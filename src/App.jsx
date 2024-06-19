2d276b14f7f0e66ac6b0b2b8f4ae1593fa663f71
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-19 12:19:46 -0300

    Create homepage.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter, Routes } from 'react-router-dom'
 import './App.css'
+import { Homepage } from './pages/Homepage/Homepage'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-
-        </Route>
+        <Route path="/" element={<Homepage />} />
         
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,7 @@
+export const Homepage = () => {
+    return (
+        <main>
+            <h1>Homepage</h1>
+        </main>
+    )
+}

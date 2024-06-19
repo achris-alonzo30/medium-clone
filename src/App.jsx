c45855e2c4458599257905d93ebf7154aa2352f1
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-06-19 12:42:35 -0300

    Add contents in the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Homepage } from './pages/Homepage/Homepage'
+import { Header } from './components/Header/Header'
 
 function App() {
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,40 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 export const Header = () => {
-    return (
-        <header>
-            <h1>
-                
-            </h1>
-        </header>
-    )
-}
+  return (
+    <header className="header">
+      <h1 className="header__title">
+        <img src="/svg/logo.svg" alt="Write Logo" width="30" height="30" />
+      </h1>
+
+      <nav className="header__nav">
+        <ul className="header__lists">
+          <li className="header__list">
+            <button className="header__write">
+                <img src="/svg/write.svg" alt="Write Logo" width="30" height="30" />
+                Write
+            </button>
+          </li>
+          <li>
+          <button>
+                <img src="/svg/bell.svg" alt="Bell Logo" width="30" height="30" />
+            </button>
+          </li>
+          <li>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
